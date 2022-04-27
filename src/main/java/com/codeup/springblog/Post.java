8e3abe489b2fa6a33bd175d93dab4c2bf5bb69d7
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-04-26 23:55:48 -0500

    working through postcontroller issue

--- src/main/java/com/codeup/springblog/Post.java
+++ src/main/java/com/codeup/springblog/Post.java
@@ -1,9 +1,20 @@
 package com.codeup.springblog;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
+    @Column(nullable = false, length = 100)
     private String title;
+
+
+    @Column(nullable = false, length = 1000)
     private String body;
 
     public Post() {
--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,11 +1,19 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class PostController {
+
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
+
     @RequestMapping(path = "/posts", method = RequestMethod.GET) //
     @ResponseBody
     public String posts() {
@@ -22,9 +30,10 @@ public class PostController {
     // EXAMPLE OF FUTURE GET-POST REQUEST
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
     @ResponseBody
-    public String createPost() {
-
-        return "Form for creating a post - pardon our dust!"; // THIS WILL BE WHERE FORM WILL BE INSERTED
+    public String createPost(Model model) {
+        model.addAttribute("newPost", new Post());
+        return "posts/create";
+//        return "Form for creating a post - pardon our dust!"; // THIS WILL BE WHERE FORM WILL BE INSERTED
     }
 
     @PostMapping("/posts/create")
